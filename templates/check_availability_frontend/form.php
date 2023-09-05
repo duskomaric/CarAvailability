0928b8a15108439ad1d589a4823e614ecd33005d
Vedran Janjic <19vedran92@gmail.com>
2023-09-05 20:27:34 +0200

    basic styles and date input type

--- templates/check_availability_frontend/form.php
+++ templates/check_availability_frontend/form.php
@@ -20,10 +20,10 @@
         </select>
         <br>
         <label for="date_out">Date Out:</label>
-        <input type="date" name="date_out" id="date_out">
+        <input type="datetime-local" name="date_out" id="date_out">
         <br>
         <label for="date_in">Date In:</label>
-        <input type="date" name="date_in" id="date_in">
+        <input type="datetime-local" name="date_in" id="date_in">
         <br>
         <button type="submit" name="check_availability_submit">Search</button>
     </form>
